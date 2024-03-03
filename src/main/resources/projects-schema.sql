ea748aa7ac3cbd27c072e8f8bf6bf03b6a33f1f5
Sarah Nichols <angelsrm@msn.com>
2024-03-02 21:36:16 -0800

    week 11 commit

--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -44,8 +44,33 @@ project_id INT NOT NULL,
 category_id INT NOT NULL,
 FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
 FOREIGN KEY (category_id) REFERENCES category (category_id)
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 1d3cff306c151d0e885e277e498b497aa48ab372
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Water plants', 0.25, 0.5, 1, 'Use the green can under the sink.');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Watering can', 1, 1.50);
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Water', 8, 0.02);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Get watering can out from under sink and fill with water', 1);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Pour water from watering can onto plants', 2);
+INSERT INTO category (category_id, category_name) VALUES (3, 'Gardening');
+INSERT INTO project_category (project_id, category_id) VALUES (1, 3);
+
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Sand bathroom door', 1.25, 1.5, 2, 'Use coarse paper then fine.');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (2, 'Coarse sandpaper', 3, 2.00);
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (2, 'Fine sandpaper', 2, 2.00);
+INSERT INTO step (project_id, step_text, step_order) VALUES (2, 'Use coarse sandpaper to get rid of roughness', 1);
+INSERT INTO step (project_id, step_text, step_order) VALUES (2, 'Use fine sandpaper to make surface smooth', 2);
+INSERT INTO category (category_id, category_name) VALUES (1, 'Doors and Windows');
+INSERT INTO project_category (project_id, category_id) VALUES (2, 1);
+
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Fix ice maker', 2, 1.5, 3, 'Figure it out.');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (3, 'Screwdriver', 1, 12.00);
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (3, 'Patience', 1, 500.00);
+INSERT INTO step (project_id, step_text, step_order) VALUES (3, 'I have no idea how to fix an ice maker', 1);
+INSERT INTO step (project_id, step_text, step_order) VALUES (3, 'Just put in a step to show that the code works', 2);
+INSERT INTO category (category_id, category_name) VALUES (2, 'Repairs');
+INSERT INTO project_category (project_id, category_id) VALUES (3, 2);
+
+;
+
